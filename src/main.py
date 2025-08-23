6c94eac7fc9c6458ee27dec0bd4d3fd9b03ae5a4
bayek0fsiwa <imvikas@tutanota.com>
2025-08-23 21:46:46 +0530

    add health check route and lifespan for app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("App starting")
+    yield
+    print("App stopping")
+    print("App stopped")
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+@app.get("/", status_code=status.HTTP_200_OK, response_model=JSONResponse)
+def health_check():
+    return JSONResponse({"status": "Online"})
