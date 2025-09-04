78fe8f0b88c8dff8245863e0a45c2eefbf5e9a58
bayek0fsiwa <imvikas@tutanota.com>
2025-09-04 15:47:49 +0530

    load new keycloak dep

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -6,9 +6,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URI: str
     API_KEY: str
-    REALM: str
-    CLIENT_ID: str
-    CLIENT_SECRET: str
+    KEYCLOAK_SERVER_URL: str
+    KEYCLOAK_REALM: str
+    KEYCLOAK_CLIENT_ID: str
+    KEYCLOAK_CLIENT_SECRET: str
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
