d1e180095e344c5c49055dfad1a37f819d45a6ca
bayek0fsiwa <imvikas@tutanota.com>
2025-09-04 15:38:22 +0530

    load keycloak configs

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -6,6 +6,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URI: str
     API_KEY: str
+    REALM: str
+    CLIENT_ID: str
+    CLIENT_SECRET: str
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
